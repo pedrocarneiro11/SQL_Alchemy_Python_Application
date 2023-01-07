a353e7c25bb998d648f72fcf7e0c1ecbd2b1308f
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2023-01-07 14:48:53 -0300

    finalizando aplicacao com o sqlAlchemy

--- integrationWithSQL/pyMongoApplication.py
+++ integrationWithSQL/pyMongoApplication.py
@@ -1,1 +1,7 @@
-import pymongo
+import pymongo as pyM
+
+client = pyM.MongoClient("mongodb+srv://pedro123:<password>@cluster2mongodbatlas.hcqqrn7.mongodb.net/test")
+
+db = client.test
+collection = db.test__collection
+print(db.test__collection)
